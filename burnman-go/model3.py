c375195fa8677e7953a3a1bdeea7404a44bede41
isabellejoosten <i.joosten@student.tudelft.nl>
2025-11-28 16:49:55 +0100

    added min and max temps to model 3 graph

--- burnman-go/model3.py
+++ burnman-go/model3.py
@@ -8,14 +8,28 @@ from model1 import g as model1_g
 from model1 import r as model1_r
 from model1 import rho as model1_rho
 from model1 import p as model1_p
-from model2 import g_mean as model2_g
-from model2 import r_mean as model2_r
-from model2 import rho_mean as model2_rho
-from model2 import p_mean as model2_p
-from model2 import T_mean as model2_T
 
-from model2 import core_mantle_boundary_pressure_min 
-from model2 import core_mantle_boundary_gravity_min, core_mantle_boundary_temp_min 
+from model2 import g_min as model2_g_min
+from model2 import g_mean as model2_g_mean
+from model2 import g_max as model2_g_max
+
+from model2 import r_min as model2_r_min
+from model2 import r_mean as model2_r_mean
+from model2 import r_max as model2_r_max
+
+from model2 import rho_min as model2_rho_min
+from model2 import rho_mean as model2_rho_mean
+from model2 import rho_max as model2_rho_max
+
+from model2 import p_min as model2_p_min
+from model2 import p_mean as model2_p_mean
+from model2 import p_max as model2_p_max
+
+from model2 import T_min as model2_T_min
+from model2 import T_mean as model2_T_mean
+from model2 import T_max as model2_T_max
+
+from model2 import core_mantle_boundary_pressure_min, core_mantle_boundary_gravity_min, core_mantle_boundary_temp_min 
 from model2 import mantle_shell_boundary_gravity_min, mantle_shell_boundary_pressure_min, mantle_shell_boundary_temp_min
 from model2 import core_boundary_min, mantle_boundary_min
 
@@ -33,58 +47,13 @@ core_material = minerals.SE_2015.bcc_iron()
 mantle_material = minerals.SLB_2022.olivine(molar_fractions=(0.5, 0.5))
 shell_material = minerals.HP_2011_ds62.h2oL()
 
-europa_min_mass = 0
-europa_mean_mass = 0
-europa_max_mass = 0
-europa_min_moi = 0
-europa_mean_moi = 0
-europa_max_moi = 0
-
-cm_bound_min = params.core_boundary
-ms_bound_min = params.mantle_boundary
-while abs(europa_min_mass-params.M_observed)/params.M_observed*100 > 0.5:
-    core_min = Layer(name='Core', radii=np.linspace(0, cm_bound_min))
-    core_min.set_material(core_material)    
-    core_min.set_temperature_mode(temperature_mode='user-defined',
+core_min = Layer(name='Core', radii=np.linspace(0, core_boundary_min))
+core_min.set_material(core_material)    
+core_min.set_temperature_mode(temperature_mode='user-defined',
                                   temperatures = np.linspace(params.core_temp_min, core_mantle_boundary_temp_min))
-    core_min.set_pressure_mode(pressure_mode='self-consistent',
+core_min.set_pressure_mode(pressure_mode='self-consistent',
                                pressure_top=core_mantle_boundary_pressure_min,
                                gravity_bottom=0)
-    core_min.make()
-
-    mantle_min = Layer(name='Mantle', radii=np.linspace(cm_bound_min, ms_bound_min))
-    mantle_min.set_material(mantle_material)
-    mantle_min.set_temperature_mode(temperature_mode='user-defined',
-                                  temperatures = np.linspace(core_mantle_boundary_temp_min, mantle_shell_boundary_temp_min))
-    mantle_min.set_pressure_mode(pressure_mode='self-consistent',
-                               pressure_top=mantle_shell_boundary_pressure_min,
-                               gravity_bottom=core_mantle_boundary_gravity_min)
-    mantle_min.make()
-
-    shell_min = Layer(name='Shell', radii=np.linspace(ms_bound_min, params.rtotal))
-    shell_min.set_material(shell_material)
-    shell_min.set_temperature_mode(temperature_mode='user-defined',
-                                  temperatures = np.linspace(mantle_shell_boundary_temp_min, params.surface_temp))
-    shell_min.set_pressure_mode(pressure_mode='self-consistent',
-                               pressure_top=0,
-                               gravity_bottom=mantle_shell_boundary_gravity_min)
-    shell_min.make()
-    europa_min = Planet('Europa (minimum core temperature)', [core_min, mantle_min, shell_min], verbose=True)
-    europa_min.make()
-
-    if europa_min.mass > params.M_observed:
-            cm_bound_min -= params.delta_r
-    elif europa_min.mass < params.M_observed:
-            cm_bound_min += params.delta_r
-    if europa_min.moment_of_inertia_factor > params.inertia_observed:
-            ms_bound_min -= params.delta_r
-    elif europa_min.moment_of_inertia_factor < params.inertia_observed:
-            ms_bound_min += params.delta_r
-
-
-
-
-
 
 core_mean = Layer(name='Core', radii=np.linspace(0, core_boundary_mean))
 core_mean.set_material(core_material)    
@@ -103,10 +72,17 @@ core_max.set_pressure_mode(pressure_mode='self-consistent',
                                gravity_bottom=0)
 
 # The "make" method does the calculations to make the pressure and gravity self-consistent.
+core_min.make()
 core_mean.make()
 core_max.make()
 
-
+mantle_min = Layer(name='Mantle', radii=np.linspace(core_boundary_min, mantle_boundary_min))
+mantle_min.set_material(mantle_material)
+mantle_min.set_temperature_mode(temperature_mode='user-defined',
+                                  temperatures = np.linspace(core_mantle_boundary_temp_min, mantle_shell_boundary_temp_min))
+mantle_min.set_pressure_mode(pressure_mode='self-consistent',
+                               pressure_top=mantle_shell_boundary_pressure_min,
+                               gravity_bottom=core_mantle_boundary_gravity_min)
 
 mantle_mean = Layer(name='Mantle', radii=np.linspace(core_boundary_mean, mantle_boundary_mean))
 mantle_mean.set_material(mantle_material)
@@ -124,9 +100,17 @@ mantle_max.set_pressure_mode(pressure_mode='self-consistent',
                                pressure_top=mantle_shell_boundary_pressure_max,
                                gravity_bottom=core_mantle_boundary_gravity_max)
 
+mantle_min.make()
 mantle_mean.make()
 mantle_max.make()
 
+shell_min = Layer(name='Shell', radii=np.linspace(mantle_boundary_min, params.rtotal))
+shell_min.set_material(shell_material)
+shell_min.set_temperature_mode(temperature_mode='user-defined',
+                                  temperatures = np.linspace(mantle_shell_boundary_temp_min, params.surface_temp))
+shell_min.set_pressure_mode(pressure_mode='self-consistent',
+                               pressure_top=0,
+                               gravity_bottom=mantle_shell_boundary_gravity_min)
 
 shell_mean = Layer(name='Shell', radii=np.linspace(mantle_boundary_mean, params.rtotal))
 shell_mean.set_material(shell_material)
@@ -144,12 +128,14 @@ shell_max.set_pressure_mode(pressure_mode='self-consistent',
                                pressure_top=0,
                                gravity_bottom=mantle_shell_boundary_gravity_max)
 
-
+shell_min.make()
 shell_mean.make()
 shell_max.make()
 
+europa_min = Planet('Europa (minimum core temperature)', [core_min, mantle_min, shell_min], verbose=True)
 europa_mean = Planet('Europa (mean core temperature)', [core_mean, mantle_mean, shell_mean], verbose=True)
 europa_max = Planet('Europa (maximum core temperature)', [core_max, mantle_max, shell_max], verbose=True)
+europa_min.make()
 europa_mean.make()
 europa_max.make()
 
@@ -185,28 +171,44 @@ for bound in bounds:
                             [bound[1], bound[1]], alpha=0.2)
 
 
-ax[0].plot(model1_r / 1.e3, model1_rho / 1.e3, linestyle='--', c='black', label='Model I')
-ax[0].plot(model2_r / 1.e3, model2_rho / 1.e3, linestyle='-', c='red', label='Model II')
-ax[0].plot(europa_mean.radii / 1.e3, europa_mean.density / 1.e3, label='Model III')
+ax[0].plot(model1_r / 1.e3, model1_rho / 1.e3, linestyle='-', c='black', label='Model I')
+ax[0].plot(model2_r_min / 1.e3, model2_rho_min / 1.e3, linestyle=':', c='red', label='Model II, min. core temp.')
+ax[0].plot(model2_r_mean / 1.e3, model2_rho_mean / 1.e3, linestyle='--', c='red', label='Model II, mean core temp.')
+ax[0].plot(model2_r_max / 1.e3, model2_rho_max / 1.e3, linestyle='-', c='red', label='Model II, max. core temp.')
+ax[0].plot(europa_min.radii / 1.e3, europa_min.density / 1.e3, linestyle=':', c='blue', label='Model III, min. core temp.')
+ax[0].plot(europa_mean.radii / 1.e3, europa_mean.density / 1.e3, linestyle='--', c='blue', label='Model III, mean core temp.')
+ax[0].plot(europa_max.radii / 1.e3, europa_max.density / 1.e3, linestyle='-', c='blue', label='Model III, max. core temp.')
 ax[0].set_ylabel('Density ($10^3$ kg/m$^3$)')
 ax[0].legend()
 
 # Make a subplot showing the calculated pressure profile
-ax[1].plot(model1_r / 1.e3, model1_p / 1.e9, c='black', linestyle='--')
-ax[1].plot(model2_r / 1.e3, model2_p / 1.e9, c='red', linestyle='-')
-ax[1].plot(europa_mean.radii / 1.e3, europa_mean.pressure / 1.e9)
+ax[1].plot(model1_r / 1.e3, model1_p / 1.e9, c='black', linestyle='-')
+ax[1].plot(model2_r_min / 1.e3, model2_p_min / 1.e9, c='red', linestyle=':')
+ax[1].plot(model2_r_mean / 1.e3, model2_p_mean / 1.e9, c='red', linestyle='--')
+ax[1].plot(model2_r_max / 1.e3, model2_p_max / 1.e9, c='red', linestyle='-')
+ax[1].plot(europa_min.radii / 1.e3, europa_min.pressure / 1.e9, c='blue', linestyle=':')
+ax[1].plot(europa_mean.radii / 1.e3, europa_mean.pressure / 1.e9, c='blue', linestyle='--')
+ax[1].plot(europa_max.radii / 1.e3, europa_max.pressure / 1.e9, c='blue', linestyle='-')
 ax[1].set_ylabel('Pressure (GPa)')
 
 # Make a subplot showing the calculated gravity profile
-ax[2].plot(model1_r / 1.e3, model1_g, c='black', linestyle='--')
-ax[2].plot(model2_r / 1.e3, model2_g, c='red', linestyle='-')
-ax[2].plot(europa_mean.radii / 1.e3, europa_mean.gravity)
+ax[2].plot(model1_r / 1.e3, model1_g, c='black', linestyle='-')
+ax[2].plot(model2_r_min / 1.e3, model2_g_min, c='red', linestyle=':')
+ax[2].plot(model2_r_mean / 1.e3, model2_g_mean, c='red', linestyle='--')
+ax[2].plot(model2_r_max / 1.e3, model2_g_max, c='red', linestyle='-')
+ax[2].plot(europa_min.radii / 1.e3, europa_min.gravity, c='blue', linestyle=':')
+ax[2].plot(europa_mean.radii / 1.e3, europa_mean.gravity, c='blue', linestyle='--')
+ax[2].plot(europa_max.radii / 1.e3, europa_max.gravity, c='blue', linestyle='-')
 ax[2].set_ylabel('Gravity (m/s$^2)$')
 ax[2].set_xlabel('Radius (km)')
 
 # Make a subplot showing the calculated temperature profile
-ax[3].plot(model2_r / 1.e3, model2_T, c='red', linestyle='-')
-ax[3].plot(europa_mean.radii / 1.e3, europa_mean.temperature)
+ax[3].plot(model2_r_min / 1.e3, model2_T_min, c='red', linestyle=':')
+ax[3].plot(model2_r_mean / 1.e3, model2_T_mean, c='red', linestyle='--')
+ax[3].plot(model2_r_max / 1.e3, model2_T_max, c='red', linestyle='-')
+ax[3].plot(europa_min.radii / 1.e3, europa_min.temperature, c='blue', linestyle=':')
+ax[3].plot(europa_mean.radii / 1.e3, europa_mean.temperature, c='blue', linestyle='--')
+ax[3].plot(europa_max.radii / 1.e3, europa_max.temperature, c='blue', linestyle='-')
 ax[3].set_ylabel('Temperature (K)')
 ax[3].set_xlabel('Radius (km)')
 ax[3].set_ylim(0.,)
